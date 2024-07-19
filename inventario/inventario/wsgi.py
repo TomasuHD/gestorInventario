80522db5f6277abde23eefdcb4a29f67a61f0890
TomasuHD <regindemonsp@gmail.com>
2024-07-19 14:08:06 -0400

    Wsgi PythonAnywhere

--- inventario/inventario/wsgi.py
+++ inventario/inventario/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for inventario project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+project_home = '/home/Tomi0806/gestorInventario/inventario'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'inventario.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventario.settings')
-
 application = get_wsgi_application()
